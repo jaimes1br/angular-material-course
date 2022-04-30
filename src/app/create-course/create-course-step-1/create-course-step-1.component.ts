6acee499a9e6d53049371e07a32e3026cdbe7cfb
Brandon Jaimes <jaimes1br@gmail.com>
2022-04-29 20:38:16 -0500

    change repo

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
 @Component({
@@ -15,12 +16,23 @@ export class CreateCourseStep1Component {
       Validators.minLength(5),
       Validators.maxLength(60)
     ]],
-    releasedAt: [new Date(), Validators.required],
+    releasedAt: [new Date(1990,0,1), Validators.required],
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+
+    if(view == 'month'){
+      return (date == 1) ? 'highlight-date' : "";
+    }
+
+    return "";
+
+  }
 
   constructor(private fb: FormBuilder) {
 
